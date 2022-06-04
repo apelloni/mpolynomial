8001ebabb7e576b1be86846f639d3cc713c5ffcc
apelloni <apelloni90@gmail.com>
2022-06-04 12:43:52 +0200

    formatting tests

--- src/parser.rs
+++ src/parser.rs
@@ -197,154 +197,159 @@ pub fn parsed_eval_fun(
     out
 }
 
-#[test]
-fn parsing_test() {
-    //let expr = INIParser::parse(Rule::calculation, "w+a/ll123 + c4^3")
-    let var_names = vec![String::from("w"), String::from("a")];
+#[cfg(test)]
+mod test {
+    use super::*;
 
-    //let expr_str = "w^0 ";
-    let expr_str = "10+a^3*(7+a-a^2)*(w+1)^0";
-    println!("input: {}", expr_str);
-    let expr_parsed = parse_expression(expr_str, var_names.as_slice());
-    println!("output: {}", expr_parsed.to_str(var_names.as_slice()));
-    assert_eq!(
-        "+10+7*a^3+a^4-a^5",
-        expr_parsed.to_str(var_names.as_slice())
-    );
-    // TEST
-    let expr_str = "1/2*(1-w/4+w*(1/2-3/4))*4+w^2*(w^23*a^2*w^5)";
-    println!("input: {}", expr_str);
-    let expr_parsed = parse_expression(expr_str, var_names.as_slice());
-    println!("output: {}", expr_parsed.to_str(var_names.as_slice()));
-    assert_eq!("+2-w^1+w^30*a^2", expr_parsed.to_str(var_names.as_slice()));
+    #[test]
+    fn parsing_test() {
+        //let expr = INIParser::parse(Rule::calculation, "w+a/ll123 + c4^3")
+        let var_names = vec![String::from("w"), String::from("a")];
 
-    let expr_str = "1/(1-45^3-4*w*w+(2*w)^2)";
-    println!("input: {}", expr_str);
-    let expr_parsed = parse_expression(expr_str, var_names.as_slice());
-    println!("output: {}", expr_parsed.to_str(var_names.as_slice()));
-    assert_eq!("-1/91124", expr_parsed.to_str(var_names.as_slice()));
+        //let expr_str = "w^0 ";
+        let expr_str = "10+a^3*(7+a-a^2)*(w+1)^0";
+        println!("input: {}", expr_str);
+        let expr_parsed = parse_expression(expr_str, var_names.as_slice());
+        println!("output: {}", expr_parsed.to_str(var_names.as_slice()));
+        assert_eq!(
+            "+10+7*a^3+a^4-a^5",
+            expr_parsed.to_str(var_names.as_slice())
+        );
+        // TEST
+        let expr_str = "1/2*(1-w/4+w*(1/2-3/4))*4+w^2*(w^23*a^2*w^5)";
+        println!("input: {}", expr_str);
+        let expr_parsed = parse_expression(expr_str, var_names.as_slice());
+        println!("output: {}", expr_parsed.to_str(var_names.as_slice()));
+        assert_eq!("+2-w^1+w^30*a^2", expr_parsed.to_str(var_names.as_slice()));
 
-    let expr_str = "0+(w)*(-1)+(-w)*(-1)+(-w)+(w)+(-2)*(-2)+(-w)+(2 + w)";
-    println!("input: {}", expr_str);
-    let expr_parsed = parse_expression(expr_str, var_names.as_slice());
-    println!("output: {}", expr_parsed.to_str(var_names.as_slice()));
-    assert_eq!("+6", expr_parsed.to_str(var_names.as_slice()));
+        let expr_str = "1/(1-45^3-4*w*w+(2*w)^2)";
+        println!("input: {}", expr_str);
+        let expr_parsed = parse_expression(expr_str, var_names.as_slice());
+        println!("output: {}", expr_parsed.to_str(var_names.as_slice()));
+        assert_eq!("-1/91124", expr_parsed.to_str(var_names.as_slice()));
 
-    let expr_str = "w^0";
-    println!("input: {}", expr_str);
-    let expr_parsed = parse_expression(expr_str, var_names.as_slice());
-    println!("output: {}", expr_parsed.to_str(var_names.as_slice()));
-    assert_eq!("+1", expr_parsed.to_str(var_names.as_slice()));
-}
+        let expr_str = "0+(w)*(-1)+(-w)*(-1)+(-w)+(w)+(-2)*(-2)+(-w)+(2 + w)";
+        println!("input: {}", expr_str);
+        let expr_parsed = parse_expression(expr_str, var_names.as_slice());
+        println!("output: {}", expr_parsed.to_str(var_names.as_slice()));
+        assert_eq!("+6", expr_parsed.to_str(var_names.as_slice()));
 
-#[test]
-fn whitespaces() {
-    //let expr = INIParser::parse(Rule::calculation, "w+a/ll123 + c4^3")
-    let var_names = vec![String::from("w"), String::from("a")];
+        let expr_str = "w^0";
+        println!("input: {}", expr_str);
+        let expr_parsed = parse_expression(expr_str, var_names.as_slice());
+        println!("output: {}", expr_parsed.to_str(var_names.as_slice()));
+        assert_eq!("+1", expr_parsed.to_str(var_names.as_slice()));
+    }
 
-    // Check spaces
-    let expr_str = "( -     2  *           w  ^  3  + 4 )";
-    let expr_parsed = parse_expression(expr_str, var_names.as_slice());
-    assert_eq!("+4-2*w^3", expr_parsed.to_str(var_names.as_slice()));
-}
+    #[test]
+    fn whitespaces() {
+        //let expr = INIParser::parse(Rule::calculation, "w+a/ll123 + c4^3")
+        let var_names = vec![String::from("w"), String::from("a")];
 
-#[test]
-fn implicit_multiplication() {
-    //let expr = INIParser::parse(Rule::calculation, "w+a/ll123 + c4^3")
-    let var_names = vec![String::from("w"), String::from("a")];
+        // Check spaces
+        let expr_str = "( -     2  *           w  ^  3  + 4 )";
+        let expr_parsed = parse_expression(expr_str, var_names.as_slice());
+        assert_eq!("+4-2*w^3", expr_parsed.to_str(var_names.as_slice()));
+    }
 
-    // Check spaces
-    let expr_str = "10a^3*2(7+a-a^2)*(w+1)^0";
-    let expr_parsed = parse_expression(expr_str, var_names.as_slice());
-    assert_eq!(
-        "+140*a^3+20*a^4-20*a^5",
-        expr_parsed.to_str(var_names.as_slice())
-    );
-    // TEST
-    let expr_str = "1/2(1-w/4+w(1/2-3/4))4+w^2*(w^23a^2w^5)";
-    let expr_parsed = parse_expression(expr_str, var_names.as_slice());
-    assert_eq!("+2-w^1+w^30*a^2", expr_parsed.to_str(var_names.as_slice()));
+    #[test]
+    fn implicit_multiplication() {
+        //let expr = INIParser::parse(Rule::calculation, "w+a/ll123 + c4^3")
+        let var_names = vec![String::from("w"), String::from("a")];
 
-    let expr_str = "( -     2             w  ^  3  + 4 )";
-    let expr_parsed = parse_expression(expr_str, var_names.as_slice());
-    assert_eq!("+4-2*w^3", expr_parsed.to_str(var_names.as_slice()));
-}
+        // Check spaces
+        let expr_str = "10a^3*2(7+a-a^2)*(w+1)^0";
+        let expr_parsed = parse_expression(expr_str, var_names.as_slice());
+        assert_eq!(
+            "+140*a^3+20*a^4-20*a^5",
+            expr_parsed.to_str(var_names.as_slice())
+        );
+        // TEST
+        let expr_str = "1/2(1-w/4+w(1/2-3/4))4+w^2*(w^23a^2w^5)";
+        let expr_parsed = parse_expression(expr_str, var_names.as_slice());
+        assert_eq!("+2-w^1+w^30*a^2", expr_parsed.to_str(var_names.as_slice()));
 
-#[test]
-fn power_as_double_star() {
-    //let expr = INIParser::parse(Rule::calculation, "w+a/ll123 + c4^3")
-    let var_names = vec![String::from("w"), String::from("a")];
+        let expr_str = "( -     2             w  ^  3  + 4 )";
+        let expr_parsed = parse_expression(expr_str, var_names.as_slice());
+        assert_eq!("+4-2*w^3", expr_parsed.to_str(var_names.as_slice()));
+    }
 
-    let expr_str = "10+a**3*(7+a-a**2)*(w+1)**0";
-    let expr_parsed = parse_expression(expr_str, var_names.as_slice());
-    //println!("input: {}", expr_str);
-    //println!("output: {}", expr_parsed.to_str(var_names.as_slice()));
-    assert_eq!(
-        "+10+7*a^3+a^4-a^5",
-        expr_parsed.to_str(var_names.as_slice())
-    );
+    #[test]
+    fn power_as_double_star() {
+        //let expr = INIParser::parse(Rule::calculation, "w+a/ll123 + c4^3")
+        let var_names = vec![String::from("w"), String::from("a")];
 
-    let expr_str = "1/2*(1-w/4+w*(1/2-3/4))*4+w**2*(w**23*a**2*w**5)";
-    let expr_parsed = parse_expression(expr_str, var_names.as_slice());
-    assert_eq!("+2-w^1+w^30*a^2", expr_parsed.to_str(var_names.as_slice()));
+        let expr_str = "10+a**3*(7+a-a**2)*(w+1)**0";
+        let expr_parsed = parse_expression(expr_str, var_names.as_slice());
+        //println!("input: {}", expr_str);
+        //println!("output: {}", expr_parsed.to_str(var_names.as_slice()));
+        assert_eq!(
+            "+10+7*a^3+a^4-a^5",
+            expr_parsed.to_str(var_names.as_slice())
+        );
 
-    let expr_str = "1/(1-45**3-4*w*w+(2*w)**2)";
-    let expr_parsed = parse_expression(expr_str, var_names.as_slice());
-    assert_eq!("-1/91124", expr_parsed.to_str(var_names.as_slice()));
+        let expr_str = "1/2*(1-w/4+w*(1/2-3/4))*4+w**2*(w**23*a**2*w**5)";
+        let expr_parsed = parse_expression(expr_str, var_names.as_slice());
+        assert_eq!("+2-w^1+w^30*a^2", expr_parsed.to_str(var_names.as_slice()));
 
-    let expr_str = "w**0";
-    let expr_parsed = parse_expression(expr_str, var_names.as_slice());
-    assert_eq!("+1", expr_parsed.to_str(var_names.as_slice()));
-}
+        let expr_str = "1/(1-45**3-4*w*w+(2*w)**2)";
+        let expr_parsed = parse_expression(expr_str, var_names.as_slice());
+        assert_eq!("-1/91124", expr_parsed.to_str(var_names.as_slice()));
 
-#[test]
-fn exact_division() {
-    let var_names = vec![
-        String::from("x1"),
-        String::from("x2"),
-        String::from("x3"),
-        String::from("x4"),
-    ];
+        let expr_str = "w**0";
+        let expr_parsed = parse_expression(expr_str, var_names.as_slice());
+        assert_eq!("+1", expr_parsed.to_str(var_names.as_slice()));
+    }
 
-    // Geometric Series
-    let mpoly_str = "(1-x1^11)^2";
-    let factor_str = "(1-x1)^2";
-    let res_str = "(1+x1+x1^2+x1^3+x1^4+x1^5+x1^6+x1^7+x1^8+x1^9+x1^10)^2";
+    #[test]
+    fn exact_division() {
+        let var_names = vec![
+            String::from("x1"),
+            String::from("x2"),
+            String::from("x3"),
+            String::from("x4"),
+        ];
 
-    let mut mpoly = parse_expression(mpoly_str, &var_names);
-    let factor = parse_expression(factor_str, &var_names);
-    let res = parse_expression(res_str, &var_names);
+        // Geometric Series
+        let mpoly_str = "(1-x1^11)^2";
+        let factor_str = "(1-x1)^2";
+        let res_str = "(1+x1+x1^2+x1^3+x1^4+x1^5+x1^6+x1^7+x1^8+x1^9+x1^10)^2";
 
-    mpoly.exact_division(&factor).unwrap();
-    assert_eq!(res.to_str(&var_names), mpoly.to_str(&var_names));
+        let mut mpoly = parse_expression(mpoly_str, &var_names);
+        let factor = parse_expression(factor_str, &var_names);
+        let res = parse_expression(res_str, &var_names);
 
-    // Multivariate
-    let mpoly_str = "(11 x1^32 + 12 x2 + 99 x3 + 21 x1*x4)^10";
-    let factor_str = "(11 x1**32 + 12 x2 + 99 x3 + 21 x1*x4)^8";
+        mpoly.exact_division(&factor).unwrap();
+        assert_eq!(res.to_str(&var_names), mpoly.to_str(&var_names));
 
-    let factor = parse_expression(factor_str, &var_names);
-    let mut mpoly = parse_expression(mpoly_str, &var_names);
+        // Multivariate
+        let mpoly_str = "(11 x1^32 + 12 x2 + 99 x3 + 21 x1*x4)^10";
+        let factor_str = "(11 x1**32 + 12 x2 + 99 x3 + 21 x1*x4)^8";
 
-    mpoly.exact_division(&factor).unwrap();
-    //println!("{}", mpoly);
-    let mut ss = String::new();
-    ss += "+9801*x3^2+2376*x2^1*x3^1+144*x2^2+4158*x1^1*x3^1*x4^1";
-    ss += "+504*x1^1*x2^1*x4^1+441*x1^2*x4^2+2178*x1^32*x3^1";
-    ss += "+264*x1^32*x2^1+462*x1^33*x4^1+121*x1^64";
-    assert_eq!(ss, format!("{}", mpoly));
-}
-#[test]
-fn overflow() {
-    let var_names = vec![String::from("x1"), String::from("x2")];
+        let factor = parse_expression(factor_str, &var_names);
+        let mut mpoly = parse_expression(mpoly_str, &var_names);
 
-    let mpoly_str = "(x1 + x2 )^66";
-    println!("Check overflow for : {}", mpoly_str);
-    let mut mpoly_a = MPolynomial::linear_pown(
-        &[num::one::<BigRational>(), num::one::<BigRational>()],
-        &[1, 2],
-        2,
-        66,
-    );
-    let mut mpoly_b = parse_expression(mpoly_str, &var_names);
-    assert_eq!(mpoly_a.to_str(&var_names), mpoly_a.to_str(&var_names));
+        mpoly.exact_division(&factor).unwrap();
+        //println!("{}", mpoly);
+        let mut ss = String::new();
+        ss += "+9801*x3^2+2376*x2^1*x3^1+144*x2^2+4158*x1^1*x3^1*x4^1";
+        ss += "+504*x1^1*x2^1*x4^1+441*x1^2*x4^2+2178*x1^32*x3^1";
+        ss += "+264*x1^32*x2^1+462*x1^33*x4^1+121*x1^64";
+        assert_eq!(ss, format!("{}", mpoly));
+    }
+    #[test]
+    fn overflow() {
+        let var_names = vec![String::from("x1"), String::from("x2")];
+
+        let mpoly_str = "(x1 + x2 )^66";
+        println!("Check overflow for : {}", mpoly_str);
+        let mut mpoly_a = MPolynomial::linear_pown(
+            &[num::one::<BigRational>(), num::one::<BigRational>()],
+            &[1, 2],
+            2,
+            66,
+        );
+        let mut mpoly_b = parse_expression(mpoly_str, &var_names);
+        assert_eq!(mpoly_a.to_str(&var_names), mpoly_a.to_str(&var_names));
+    }
 }
